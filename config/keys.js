bb145f5ff536d04ea099044691acc2abc58053e7
Tirthankar Kundu <tirthankar.kundu94@gmail.com>
2018-08-13 03:51:01 +0530

    Added user functionality

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://tirth:tirth123@ds119572.mlab.com:19572/devconnector"
+  mongoURI: "mongodb://tirth:tirth123@ds119572.mlab.com:19572/devconnector",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
@@ -21,6 +27,11 @@ app.get("/", (req, res) => {
   console.log("Request accepted");
   res.send("Hello");
 });
+//Passprt middleware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
 
 //Use Routes
 app.use("/api/users", users);
